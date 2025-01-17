4cb458c833b242c891ace55d9a780c259db30f81
komal1157 <navalekomal981@gmail.com>
2025-01-17 19:00:37 +0530

    file creating example

--- src/FileHandling/Example.java
+++ src/FileHandling/Example.java
@@ -6,53 +6,21 @@ import java.io.IOException;
 import java.io.FileWriter;
 import java.io.BufferedWriter;
 
-class Emp{
-	int id;
-	String name;
-	int age;
-	public Emp(int i,String n,int a) {
-		
-		id=i;
-		name=n;
-		age=a;
-	}
-	public String toString() {
-		return ("empid "+id+" name "+name+" age "+age);
-	}
-}
-
 public class Example {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       Scanner sc=new Scanner(System.in);
-       File file=new File("w.txt");
-       
-       Emp e1=new Emp()
-       
-       try {
-    	   if(!file.exists()) {
-    	   file.createNewFile();
-    	   System.out.println("file created");
-    	   }else {
-    		   System.out.println("File already exists");  
-    	   }
-       }
-       catch(IOException obj) {
-    	   obj.printStackTrace();
-       }
-       
-       try {
-    	   
-    	   FileWriter fw=new FileWriter(file);
-    	   BufferedWriter bw=new BufferedWriter(fw);
-    	   bw.append(obj.toString());
-    	   
-    	   bw.close();
-       }
-       catch(IOException obj2){
-    	   obj2.printStackTrace();
-       }
+        
+		File file=new File("book.txt");
+		try {
+			if(!file.exists()) {
+				file.createNewFile();
+				System.out.println(" new file created successfully !");
+				}else {
+					System.out.println("file already exist !");
+				}
+		}catch(IOException o) {
+			o.printStackTrace();
+		}
 	}
-
 }
